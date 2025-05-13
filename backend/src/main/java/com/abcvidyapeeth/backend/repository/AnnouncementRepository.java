817ada842caeadf821823b6fa7231a9ee7664835
Shashank Maddheshiya <164163365+Maddheshiya-29@users.noreply.github.com>
2025-05-13 12:37:31 +0530

    deleted backend old code

--- backend/src/main/java/com/abcvidyapeeth/backend/repository/AnnouncementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abcvidyapeeth.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.abcvidyapeeth.backend.entity.Announcement;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-}
